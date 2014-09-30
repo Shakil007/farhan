d83c4bb26ea242c0ee3ab8f4600e628c07c3b42b
Shakil007 <mehedi_nsu07@hotmail.com>
2014-09-30 06:46:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farhan::Application.config.secret_token = 'd75c2220c24b35040af902182f8c19f9fec812cd11984c1b5401f3a6676261d79231ca32d4cc7f080a9c90d1ad3d9dcef4bd6daa404b9d3fc7c41437c5ce5005'
